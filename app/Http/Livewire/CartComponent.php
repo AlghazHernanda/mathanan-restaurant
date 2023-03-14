8a80546b28824908d62d156999fba0795efe46ba
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-15 02:17:45 +0700

    cartcomponent

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,41 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CartComponent extends Component
 {
+    // untuk menambah quantity
+    public function increaseQuantity($rowId)
+    {
+        $menu = Cart::get($rowId);
+        $qty = $menu->qty + 1;
+        Cart::update($rowId, $qty);
+        $this->emitTo('cart-icon-component', 'refreshComponent');
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        $menu = Cart::get($rowId);
+        $qty = $menu->qty - 1;
+        Cart::update($rowId, $qty);
+        $this->emitTo('cart-icon-component', 'refreshComponent'); //memanggil refreshComponent di CartIconComponent
+    }
+
+    //untuk delete item di cart
+    public function destroy($id)
+    {
+        Cart::remove($id);
+        session()->flash('success_message', 'Item has been removed');
+        $this->emitTo('cart-icon-component', 'refreshComponent'); //memanggil refreshComponent di CartIconComponent
+    }
+
+    public function clearAll()
+    {
+        Cart::destroy();
+        $this->emitTo('cart-icon-component', 'refreshComponent'); //memanggil refreshComponent di CartIconComponent
+    }
+
     public function render()
     {
         return view('livewire.cart-component');
